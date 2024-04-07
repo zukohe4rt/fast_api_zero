fdfb81c166faa580c6fe6c212d5916ce3f4c5354
zuko <zukohe4rt@gmail.com>
2024-04-07 00:40:58 -0300

    implementing crud routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,53 @@
-from fastapi.testclient import TestClient
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'sophia',
+            'email': 'sophia@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'sophia',
+        'email': 'sophia@example.com',
+        'id': 1,
+    }
 
-from fast_zero.app import app
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'sophia',
+                'email': 'sophia@example.com',
+                'id': 1,
+            }
+        ]
+    }
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app)
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
 
-    response = client.get('/')
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {'message': 'User deleted'}
